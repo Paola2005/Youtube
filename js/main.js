bad43ed1d1c1134c79493f4b39f3bdc6f8e463da
Paola2005 <velascopaola2005@gmail.com>
2023-08-29 17:10:21 -0500

    feat :clown_face: some styles and structures change some styles and structures

--- js/main.js
+++ js/main.js
@@ -1,94 +1,15 @@
-let todo = async() =>{
-const chanel = await fetch("chanel.json");
-    let video = await fetch("videos.json");
-    let videos = await video.json();
-    let canal = await chanel.json();
-    const creativeCodeURL = 'UC8fkwsjcI_MhralEX1g4OBw';
-    let nav=document.querySelector(".primero")
-    nav.insertAdjacentHTML("beforeend",/*HTML*/`
-    <div class="nose primero">
-    <button id="menuIcon"><img src="${canal.ima}" class="iconom"></button>
-    <img src="${canal.logo}" class="logo">
-        </div>
-            <!--nav-middle-->
-            <div class="nose2 primero">
-                    <div class="buscador primero">
-                        <input type="text" placeholder="Search">
-                        <img src="${canal.logserch}">
-                    </div>
-                    <img src="${canal.logmic}" class="micro">
-            </div>
-            <!--nav-right-->
-            <div class="nose3 primero">
-            ${canal.logos2.map((value) => /*html */`
-                <img src="${value.log2}" ><br>`).join(" ")}
-                <img src="${canal.log1}" class="usuario">
-            </div>
 
-    `)
-    const menuIcon = document.getElementById('menuIcon');
-const lado = document.querySelector('.segundo');
-const container = document.querySelector('.cotainer');
-let ladoVisible = false;
+var menuicon=document.querySelector(".iconom");
+var sidebar=document.querySelector(".segundo");
+var container=document.querySelector(".cotainer");
 
-menuIcon.addEventListener('click', function() {
-  if (ladoVisible) {
-    lado.style.left = "-300px";
-    container.classList.remove('with-sidebar');
-  } else {
-    lado.style.left = "0px";
-    container.classList.add('with-sidebar');
-  }
-  ladoVisible = !ladoVisible;
-});
-
-    let lateral=document.querySelector(".segundo")
-    lateral.insertAdjacentHTML("beforeend",/*HTML*/`
-    ${canal.textoslate1.map((value) => /*html */`
-    <div class="links">
-    <a ><img  src="${value.imagenes1}"><p>${value.cosa}</p></a>
-            </div>`).join("")}
-            <hr>
-            <div class="suscriptores">
-            <h3>Subscribed</h3>
-            <div>
-            ${canal.textoslate2.map((value) => /*html */`
-            <div class="suscriptores">
-            <a href=""><img src="${value.imagenes2}" ><P>${value.cosa2}</P></a>
-                </div>`).join("")}
-        
-            </div>
-
-    `)
-    let banner = document.querySelector(".cotainer")
-    console.log(videos.contents[0].video.movingThumbnails[0].url);
-        banner.insertAdjacentHTML("beforeend", /*HTML*/`
-        <div class="banner">
-            <img src="${canal.banner.desktop[3].url}" class="bannerlar">
-        </div>
-        <div class="listacontenedor">
-            ${videos.contents.map((value) => 
-                /*HTML*/`
-                <div class="videos" idvideo="21">
-                    <a href="playvideo.html"><img src="${value.video.thumbnails[3].url}" class="imagenprin"></a>
-                        <div class="primero">
-                            <div class=vid>
-                                <img src="${canal.avatar[1].url}">
-                            </div>
-
-                            <div class="infovideo">
-                                <a href="playvideo.html">${value.video.title}</a>
-                                <p>${canal.title}</p>
-                                <p>${value.video.stats.views} views ·${value.video.publishedTimeText}</p>
-                            </div>
-                        </div>
-                    </div>
-                    `).join("")}
-        </div>
-    `);
-    
+menuicon.onclick=function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
 }
-todo()
+
+
+
 
 const path= "videos";
 (async(ids)=>{ 
@@ -97,7 +18,7 @@ const path= "videos";
         console.log(response);
 
 
-        let myVideos = document.querySelector('#contenedorvids');
+        let myVideos = document.querySelector('.listacontenedor');
 
         myVideos.insertAdjacentHTML("beforeend", `
             ${response.contents.map((value)=>`
--- js/play.js
+++ js/play.js
@@ -1,53 +1,13 @@
 
+var menuicon=document.querySelector(".iconom");
+var sidebar=document.querySelector(".segundo");
+var container=document.querySelector(".cotainer");
 
+menuicon.onclick=function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
 
-let todo = async() =>{
-    const chanel = await fetch("chanel.json");
-        let video = await fetch("videos.json");
-        let videos = await video.json();
-        let canal = await chanel.json();
-    
-        let nav=document.querySelector(".primero")
-        nav.insertAdjacentHTML("beforeend",/*HTML*/`
-        <div class="nose primero">
-        <a href="../index.html"><img src="${canal.logo}" class="logo"></a>
-            </div>
-                <!--nav-middle-->
-                <div class="nose2 primero">
-                        <div class="buscador primero">
-                            <input type="text" placeholder="Search">
-                            <img src="${canal.logserch}">
-                        </div>
-                        <img src="${canal.logmic}" class="micro">
-                </div>
-                <!--nav-right-->
-                <div class="nose3 primero">
-                ${canal.logos2.map((value) => /*html */`
-                    <img src="${value.log2}" ><br>`).join(" ")}
-                    <img src="${canal.log1}" class="usuario">
-                </div>
-    
-        `)
-        
-    
-        let lateral=document.querySelector(".segundo")
-        lateral.insertAdjacentHTML("beforeend",/*HTML*/`
-        ${canal.textoslate1.map((value) => /*html */`
-        <div class="links">
-        <a ><img  src="${value.imagenes1}"><p>${value.cosa}</p></a>
-                </div>`).join("")}
-                <hr>
-                <div class="suscriptores">
-                <h3>Subscribed</h3>
-                <div>
-                ${canal.textoslate2.map((value) => /*html */`
-                <div class="suscriptores">
-                <a href=""><img src="${value.imagenes2}" ><P>${value.cosa2}</P></a>
-                    </div>`).join("")}
-            
-                </div>
-    
-        `)
 
         // let viderec=document.querySelector(".derecha")
         // viderec.insertAdjacentHTML("beforeend",/*HTML*/`
@@ -60,7 +20,7 @@ let todo = async() =>{
         //             <p>${value.video.stats.views} Views &bull; ${value.video.publishedTimeText}</p>
         //         </div>
         // </div>
-    
+
         // `).join("")}
         // `)}
         // const videoElements = document.querySelectorAll('.videolista');
@@ -73,10 +33,7 @@ let todo = async() =>{
 
 
 
-            
-            
-    };
-todo()
+
 
 function changingVideo(ids){
     let iframe = document.querySelector('.play-video');
@@ -84,7 +41,7 @@ function changingVideo(ids){
     <iframe width="100%" height="615" src="https://www.youtube.com/embed/${ids}?si=czx-JXcyfxDxe0lv&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     `)
 }
-    
+
 
 
 let storageElement = localStorage.getItem('ID')
@@ -92,25 +49,26 @@ console.log(storageElement);
 changingVideo(storageElement)
 
 
-const url = `https://youtube138.p.rapidapi.com/video/details/?id=${storageElement}&hl=en&gl=US`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b595005c91mshe1eb93b5d95b243p1ea044jsn23aae979be4a',
-		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-	}
-};
+  const url = `https://youtube138.p.rapidapi.com/video/details/?id=${storageElement}&hl=en&gl=US`;
+  const options = {
+  	method: 'GET',
+  	headers: {
+  		'X-RapidAPI-Key': 'dadc31c17fmsh05d55cc1726e60bp1680b4jsn052873b35ae9',
+  		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+  	}
+  };
+
 
 // let infoVid = 'videoInfo';
 let info = async() =>{
-let peticion = await fetch (url,options) 
+let peticion = await fetch (url,options)
 let response = await peticion.json()
 console.log(response);
 
 let infoVid = document.querySelector('#top-info')
 
 if (response.description == null){
-    infoVid.insertAdjacentHTML('afterend', 
+    infoVid.insertAdjacentHTML('afterend',
 `
     <h3>${response.title}</h3>
 
@@ -140,7 +98,7 @@ if (response.description == null){
 `)
 }
 else{
-    infoVid.insertAdjacentHTML('afterend', 
+    infoVid.insertAdjacentHTML('afterend',
     `
         <h3>${response.title}</h3>
 
@@ -162,7 +120,7 @@ else{
             </div>
             <button type="button">Subscribe</button>
         </div>
-        
+
         <div class="descripvideos" id="descripvideos">
             <p>${response.description}</p>
             <hr>
@@ -173,23 +131,22 @@ else{
 info(url,options);
 
 
-const token = 'b595005c91mshe1eb93b5d95b243p1ea044jsn23aae979be4a'
-const urln = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
-const optionse = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': `${token}`,
-		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-	}
-};
+  const urln = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
+  const optionse = {
+  	method: 'GET',
+  	headers: {
+  		'X-RapidAPI-Key': 'dadc31c17fmsh05d55cc1726e60bp1680b4jsn052873b35ae9',
+  		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+  	}
+  };
 
 
 
          (async(parameters,videos) => {
-            let petic = await fetch (parameters,videos) 
+            let petic = await fetch (parameters,videos)
             let repon = await petic.json()
             console.log(repon);
-   
+
             // INSERTAR TARJETAS DE VIDEO AL PLAY-VIDEO
             let rightSide = document.querySelector('.derecha')
             rightSide.insertAdjacentHTML('beforeend', `
@@ -204,7 +161,7 @@ const optionse = {
                 </div>
                 `).join("")}
             `)
-     // FUNCION DE QUE ESCUCHARÁ TODAS LAS TARJETAS DE VIDEOS CREADOS AL HACERLE CLICK 
+     // FUNCION DE QUE ESCUCHARÁ TODAS LAS TARJETAS DE VIDEOS CREADOS AL HACERLE CLICK
             const videoElements = document.querySelectorAll('.videolista');
             // Agrega un manejador de eventos a cada tarjeta video
             videoElements.forEach(video => {
@@ -218,5 +175,4 @@ const optionse = {
         })(urln,optionse)
 
 
-        
-        
+
